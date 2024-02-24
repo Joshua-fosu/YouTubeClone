634e52eb0d3d26384e353b005cd3a5cb2492fd20
Joshua-fosu <joshuafosu119@gmail.com>
2024-02-24 11:32:17 -0500

    Implemented google cloud storage functions for downloading and processing videos

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,21 +1,64 @@
 import express from "express";
-import Ffmpeg from "fluent-ffmpeg";
+
+import {
+    uploadProcessedVideoFolder,
+    downloadRawVideo,
+    deleteProcessedLocalFolder,
+    deleteRawLocalFile,
+    convertVideo,
+    setupDirectories
+} from './storage'
+
+//Create the local directories
+setupDirectories();
 
 const app = express();
 app.use(express.json())
 
-app.post("/process-video", (req, res) => {
-    const inputFilePath = req.body.inputFilePath;
+// Process a video file from Cloud Storage into multiple resolutions and attach thumbnails
+app.post('/process-video', async (req, res) => {
 
-    if (!inputFilePath) {
-        res.status(400).send("Bad Request: Missing file path.")
+    // Get the bucket and filename from the Cloud Pub/Sub message
+    let data;
+    try {
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+        data = JSON.parse(message);
+        if (!data.name) {
+            throw new Error('Invalid message payload received.');
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send('Bad Request: missing filename.');
     }
 
-    
+    const inputFileName = data.name;
+    const outputFolderName = `processed-${inputFileName}`;
 
+    // Download the raw video from Cloud Storage
+    await downloadRawVideo(inputFileName);
 
+    // Process the video into 360p
+    try {
+        await convertVideo(inputFileName, outputFolderName)
+    } catch (err) {
+        await Promise.all([
+            deleteRawLocalFile(inputFileName),
+            deleteProcessedLocalFolder(outputFolderName)
+        ]);
+        return res.status(500).send('Processing failed');
+    }
 
-})
+    // Upload the processed video to Cloud Storage
+    await uploadProcessedVideoFolder(outputFolderName);
+
+    await Promise.all([
+        deleteRawLocalFile(inputFileName),
+        deleteProcessedLocalFolder(outputFolderName)
+    ]);
+
+    return res.status(200).send('Processing finished successfully');
+});
+
 
 
 const port = process.env.PORT || 3000;
--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -1,4 +1,4 @@
-import { Storage } from "@google-cloud/storage";
+import { Storage, TransferManager } from "@google-cloud/storage";
 import fs from 'fs';
 import Ffmpeg from "fluent-ffmpeg";
 
@@ -7,6 +7,8 @@ const storage = new Storage()
 const rawVideoBucketName = "sb-google-pm-rw-vid";
 const processedVideoBucketName = "sb-google-pm-processed-vid";
 
+const transferManager = new TransferManager(storage.bucket(processedVideoBucketName))
+
 const localRawVideoPath = "./raw-videos";
 const localProcessedVideoPath = "./processed-videos";
 
@@ -14,7 +16,8 @@ const localProcessedVideoPath = "./processed-videos";
  * Creates local directories for raw and processed videos
  */
 export function setupDirectories() {
-
+    ensureDirectoryExistence(localRawVideoPath);
+    ensureDirectoryExistence(localProcessedVideoPath);
 }
 
 /**
@@ -29,7 +32,7 @@ export function convertVideo(rawVideoName: string, processedVideoName: string) {
     const inputFilePath = `${localRawVideoPath}/${rawVideoName}`;
     return Promise.all(resolutions.map((resolution) => {
         return new Promise<void>((resolve, reject) => {
-            const outputFilePath = rawVideoName.replace(/(\.[\w\d_-]+)$/i, `-${resolution.suffix}$1`);
+            const outputFilePath = processedVideoName.replace(/(\.[\w\d_-]+)$/i, `-${resolution.suffix}$1`);
             const outputPath = `${localProcessedVideoPath}/${processedVideoName}/${outputFilePath}`;
             Ffmpeg(inputFilePath)
                 .outputOptions("-vf", `scale=-1:${resolution.size}`)
@@ -74,3 +77,63 @@ export async function downloadRawVideo(fileName: string) {
 }
 
 
+export async function uploadProcessedVideoFolder(folderName: string) {
+    const bucket = storage.bucket(processedVideoBucketName);
+
+    // Uploads the directory
+    await transferManager.uploadManyFiles(folderName)
+        .then(() => {
+            console.log(`${folderName} uploaded successfully.`);
+        })
+        .catch((err) => {
+            console.log("Error occured when uploading folder")
+        });
+}
+
+export async function deleteProcessedLocalFolder(folderPath: string) {
+    return new Promise<void>((resolve, reject) => {
+        if (fs.existsSync(folderPath)) {
+            fs.rm(folderPath, { recursive: true, force: true }, (err) => {
+                if (err) {
+                    console.log("Failed to delete folder");
+                    reject(err);
+                } else {
+                    console.log("Folder deleted successfully");
+                    resolve();
+                }
+            });
+        } else {
+            console.log("Folder not found, skipping deletion");
+            resolve();
+        }
+    });
+}
+
+export async function deleteRawLocalFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(filePath)) {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(`Failed to delete file at ${filePath}`, err);
+                    reject(err);
+                } else {
+                    console.log(`File deleted at ${filePath}`);
+                    resolve();
+                }
+            });
+        } else {
+            console.log(`File not found at ${filePath}, skipping delete.`);
+            resolve();
+        }
+    });
+}
+
+/**
+ * 
+ */
+function ensureDirectoryExistence(dirPath: string) {
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true }); // recursive: true enables creating nested directories
+        console.log(`Directory created at ${dirPath}`);
+    }
+}
